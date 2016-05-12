6fb3508d58d1dd9b132e88337443f263fbae0161
deminf <daupaiev@gmail.com>
2016-05-12 20:41:22 +0300

    s09 solution

---
+++ src/s09.java
@@ -0,0 +1,21 @@
+public class s09 {
+
+    public static void main (String[] args){
+        System.out.println(new s09().run());
+    }
+
+    private static int number = 1000;
+
+    public String run(){
+        for (int i = 1; i < number; i++){
+            for (int j = i + 1; j < number; j++){
+                int k = number - i - j;
+                if (i * i + j * j == k * k){
+                    return Integer.toString(i * j * k);
+                }
+            }
+        }
+        throw new AssertionError("Not found");
+    }
+
+}
